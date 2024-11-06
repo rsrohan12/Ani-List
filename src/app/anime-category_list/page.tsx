554b9e596fd5f8501c66cff0011959ddfc49800b
Rohan <rsrohan787@gmail.com>
2024-11-06 13:16:00 +0530

    all set!!

--- src/app/anime-category_list/page.tsx
+++ src/app/anime-category_list/page.tsx
@@ -2,8 +2,7 @@ import AnimeCategory from "@/components/AnimeCategory";
 
 export default function Page() {
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-4xl font-bold mb-8 text-center">Anime Search</h1>
+    <div>
       <AnimeCategory />
     </div>
   )
